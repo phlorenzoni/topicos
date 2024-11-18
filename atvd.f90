fbd7ec109cc4bcf68ef9ec960604725bfef478a8
Paulo Henrique <78452260+phlorenzoni@users.noreply.github.com>
2024-11-18 19:40:12 -0300

    adiciona arquivo f90

---
+++ atvd.f90
@@ -0,0 +1,42 @@
+program atividade_1
+    use, intrinsic :: iso_fortran_env, only: sp => real32, dp => real64, i4 => int32, i8 => int64
+    use statistical_tools
+    implicit none
+    integer(i8), parameter :: Ndados = 60
+    real(dp) :: x(Ndados),y(Ndados), media_, var_,des 
+    real(dp), allocatable :: media_mov(:)
+    character (len=100) :: fname
+    
+    fname='corrente.dat'
+    call leitura_dados(Ndados,x,y,fname)
+
+    
+    media_= media(Ndados, y)
+    var_=variancia(Ndados,y)
+    des = desvio_padrao(var_)
+
+   write(*,'(A,2x,f5.3)') "Media:", media_
+   write(*,'(A,2x,f5.3)') "Variancia:", var_
+   write(*,'(A,2x,f5.3)') "Desvio padrao", des
+
+contains
+
+subroutine leitura_dados(Ndados, x, y, nome_arq)
+!Vetores que receberão os dados dos eixos x e y
+integer(i8) :: Ndados
+real(dp) :: x (Ndados), y(Ndados)
+integer :: arquivo, i 
+character(len=100) :: nome_arq
+
+! Abre o arquivo
+open(unit=arquivo, file=fname)
+! Faz a leitura
+do i = 1, Ndados
+    read(arquivo, *) x(i), y(i)
+enddo
+
+! Fecha o arquivo
+close(arquivo)
+
+endsubroutine
+end program
